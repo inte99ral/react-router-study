fc87f2bc04996ff7ee1791d867f7d20b5b3894fc
박준혁 <inte99ral@gmail.com>
2022-10-18 18:01:50 +0900

    22-10-18.feat.router-setting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div style={{ padding: 20, border: "5px solid gray" }}>
+          <Link to="/">홈</Link>
+          <br />
+          <Link to="/photo">사진</Link>
+          <br />
+          <Link to="/rooms">방 소개</Link>
+
+          <Routes>
+            <Route path="/" component={Home} />
+            <Route path="/photo" component={Photo} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
+}
+
+function Home({ match }) {
+  return <h2>여기는 홈페이지입니다.</h2>;
+}
+function Photo({ match }) {
+  return <h2>여기는 사진첩입니다.</h2>;
 }
 
 export default App;
